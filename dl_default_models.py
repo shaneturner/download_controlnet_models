de7b8b85dc492d496853bd50ff9a485346ee7ac8
Shane Turner <shaneturner@users.noreply.github.com>
2023-11-09 16:07:06 +1300

    Move core script to main() function

--- dl_default_models.py
+++ dl_default_models.py
@@ -36,58 +36,64 @@ def get_filename_from_response(response):
                 return unquote(sanitized_filename)
     return None
 
-for url in file_urls:
-    # Get the filename from the URL
-    remote_filename = os.path.basename(urlparse(url).path)
+def main():
+    for url in file_urls:
+        # Get the filename from the URL
+        remote_filename = os.path.basename(urlparse(url).path)
 
-    # Define the complete path for the local file
-    local_file_path = os.path.join(destination_folder, remote_filename)
+        # Define the complete path for the local file
+        local_file_path = os.path.join(destination_folder, remote_filename)
 
-    # Check if the file with the same name exists locally
-    if os.path.isfile(local_file_path):
-        # Send a HEAD request to the URL to get remote file info
-        with requests.head(url, allow_redirects=True) as response:
+        # Check if the file with the same name exists locally
+        if os.path.isfile(local_file_path):
+            # Send a HEAD request to the URL to get remote file info
+            with requests.head(url, allow_redirects=True) as response:
+                if response.status_code == 200:
+                    # Get the remote file size
+                    remote_size = int(response.headers.get('content-length', 0))
+                    # Get the local file size
+                    local_size = os.path.getsize(local_file_path)
+
+                    if remote_size == local_size:
+                        print(f'{remote_filename} already exists and is the same size. Skipping. \n')
+                        continue  # Skip downloading
+                    else:
+                        print(f'{remote_filename} exists but has a different size. Downloading...')
+
+        # Send a GET request to the URL and follow redirects
+        with requests.get(url, stream=True, allow_redirects=True) as response:
             if response.status_code == 200:
-                # Get the remote file size
-                remote_size = int(response.headers.get('content-length', 0))
-                # Get the local file size
-                local_size = os.path.getsize(local_file_path)
+                # Get the filename from the server response
+                file_name = get_filename_from_response(response)
 
-                if remote_size == local_size:
-                    print(f'{remote_filename} already exists and is the same size. Skipping. \n')
-                    continue  # Skip downloading
+                if file_name is not None:
+                    # Define the complete path for the file
+                    file_path = os.path.join(destination_folder, file_name)
+
+                    # Display file name to download
+                    print(f'File:  {file_name}')
+
+                    # Initialize a progress bar
+                    total_size = int(response.headers.get('content-length', 0))
+                    progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
+
+                    # Download and save the file with streaming and progress display
+                    with open(file_path, 'wb') as file:
+                        for data in response.iter_content(chunk_size=8192):
+                            file.write(data)
+                            progress_bar.update(len(data))
+
+                    progress_bar.close()
+                    print(f'{file_name} downloaded successfully to {destination_folder}\n')
                 else:
-                    print(f'{remote_filename} exists but has a different size. Downloading...')
-
-    # Send a GET request to the URL and follow redirects
-    with requests.get(url, stream=True, allow_redirects=True) as response:
-        if response.status_code == 200:
-            # Get the filename from the server response
-            file_name = get_filename_from_response(response)
-
-            if file_name is not None:
-                # Define the complete path for the file
-                file_path = os.path.join(destination_folder, file_name)
-
-                # Display file name to download
-                print(f'File:  {file_name}')
-
-                # Initialize a progress bar
-                total_size = int(response.headers.get('content-length', 0))
-                progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
-
-                # Download and save the file with streaming and progress display
-                with open(file_path, 'wb') as file:
-                    for data in response.iter_content(chunk_size=8192):
-                        file.write(data)
-                        progress_bar.update(len(data))
-
-                progress_bar.close()
-                print(f'{file_name} downloaded successfully to {destination_folder}\n')
+                    print(f'Failed to extract filename from {url}\n')
             else:
-                print(f'Failed to extract filename from {url}\n')
-        else:
-            print(f'Failed to download {url}\n')
+                print(f'Failed to download {url}\n')
 
-# All files have been checked and downloaded as needed
-print('\nAll files checked and downloaded as needed.\n')
+    # All files have been checked and downloaded as needed
+    print('\nAll files checked and downloaded as needed.\n')
+
+
+if __name__ == "__main__":
+    main()
+    
